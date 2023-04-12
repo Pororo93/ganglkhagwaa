bb6278b161c1b10c600f1c293f6a1540bf38fc1e
Admin <Admin@DESKTOP-E8KMHUG>
2023-04-12 13:55:30 +0800

    lkhagwaagiin aaw oorchlolt oruulaw

---
+++ src/tse/lab14.java
@@ -0,0 +1,16 @@
+package tse;
+
+public class lab14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			   //statements that may cause an exception
+			}catch(Exception e){
+			  //statements that will execute if exception occurs
+			}finally{
+			  //statements that execute whether the exception occurs or not
+			}    
+	}
+
+}
